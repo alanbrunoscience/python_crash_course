52ea46bce84d9b301b6df0099f971d2f66b984d3
alanbrunoscience <alanbrunomath@gmail.com>
2024-07-29 22:22:33 -0300

    Chapter 6 exercises solution

---
+++ Chapter 6 - Dictionaries/ex1_alien.py
@@ -0,0 +1,11 @@
+# A dictionary in Python is a collection of key-value pairs.
+# Each key is connected to a value, and you can use a key to access
+# the value associated with that key.
+
+alien_0 = {'color': 'green', 'points': 5}
+
+print(alien_0['color'])
+print(alien_0['points'])
+
+new_points = alien_0['points']
+print(f"You just earned {new_points} points!")
